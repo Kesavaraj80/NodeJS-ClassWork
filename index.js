91d4dbd55dfbc0acb344ca0bdd7ee5007de4753d
Kesavaraj80 <mkesavaraj1998@gmail.com>
2021-12-05 16:21:00 +0530

    cors added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import {MongoClient} from 'mongodb';
 import dotenv from "dotenv";
 import { moviesRouter } from './routes/movies.js';
 import { usersRouter } from './routes/users.js';
+import cors from "cors";
 // import bcrypt from "bcrypt";
 // import { getMovies, 
 //     getMovieById, 
@@ -21,6 +22,8 @@ const PORT = process.env.PORT||9000;
 const MONGO_URL = process.env.MONGO_URL;
 
 app.use(express.json());
+app.use(cors())
+
 
 async function createConnection(){
     const client = new MongoClient(MONGO_URL);
