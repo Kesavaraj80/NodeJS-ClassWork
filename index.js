dd4bc30790d71fa44afe9a8bb0aa2e76780520d2
Kesavaraj80 <mkesavaraj1998@gmail.com>
2021-12-12 11:56:04 +0530

    recipe added

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ const MONGO_URL = process.env.MONGO_URL;
 app.use(express.json());
 app.use(cors())
 
+const receipies = [{
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg?quality=90&webp=true&resize=375,341",
+    name: "Chorizo & mozzarella gnocchi bake"
+  },
+  {
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/butter-chicken-cf6f9e2.jpg?quality=90&webp=true&resize=375,341",
+    name: "Easy butter chicken"
+  },
+  {
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/classic-lasange-4a66137.jpg?quality=90&webp=true&resize=375,341",
+    name: "Easy classic lasagne"
+  },
+  {
+    imageurl: "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/easy-teriyaki-c845724.jpg?quality=90&webp=true&resize=375,341",
+    name: "Easy teriyaki chicken"
+  }];
 
 async function createConnection(){
     const client = new MongoClient(MONGO_URL);
@@ -45,13 +61,23 @@ app.get('/', (req, res) => {
 });
 
 
-
-
-
 app.use('/movies', moviesRouter);
 
 app.use('/users', usersRouter)
 
+
+// recipes
+
+app.get('/recipes',async (req, res)=>{
+    const result = await client.db("movieDb").collection("recipes").find().toArray();
+    res.send(result)
+})
+
+app.post('/recipes',async (req, res)=>{
+    const data = req.body;
+    const result = await client.db("movieDb").collection("recipes").insertMany(data);
+    res.send(result)
+})
 
 app.listen(PORT, () => console.log("App is Started"));
 
